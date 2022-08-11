51798ae22c9abacc6c914c793164096753874b7a
Jaehyun Kim <Jaehyun8719@gmail.com>
2022-08-11 20:21:24 +0900

    update

---
+++ src/main/kotlin/basic/Chapter1.kt
@@ -0,0 +1,15 @@
+package basic
+
+fun main() {
+
+//    var number1 = 10L // Variable의 약자 /발
+//    val number2 = 10L // Value의 약자 / 밸
+
+    // Primitive Type
+//    var number1 = 10L
+//    var number3 = 1_000L
+
+    // nullable
+    var number3: Long? = 1_000L
+    number3 = null
+}
